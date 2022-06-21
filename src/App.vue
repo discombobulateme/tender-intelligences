7dad494cb61ea66970e2da23eaa46aa2e7a77176
discombobulateme <paloma.oliveira@saucelabs.com>
2022-06-21 13:17:12 +0200

    Delete twitter logo

--- src/App.vue
+++ src/App.vue
@@ -20,22 +20,6 @@ import MediaKit from "@/views/MediaKit.vue";
       <li data-menuanchor="page5"><a href="#page5">events</a></li>
       <li data-menuanchor="page6"><a href="#page6">contact</a></li>
       <li data-menuanchor="page7"><a href="#page7">media kit</a></li>
-      <li>
-        <a
-          href="https://twitter.com/imac2"
-          target="_blank"
-          rel="noopener"
-          class="twitter-share"
-        >
-          <i>
-            <svg viewBox="0 0 512 512">
-              <path
-                d="M419.6 168.6c-11.7 5.2-24.2 8.7-37.4 10.2 13.4-8.1 23.8-20.8 28.6-36 -12.6 7.5-26.5 12.9-41.3 15.8 -11.9-12.6-28.8-20.6-47.5-20.6 -42 0-72.9 39.2-63.4 79.9 -54.1-2.7-102.1-28.6-134.2-68 -17 29.2-8.8 67.5 20.1 86.9 -10.7-0.3-20.7-3.3-29.5-8.1 -0.7 30.2 20.9 58.4 52.2 64.6 -9.2 2.5-19.2 3.1-29.4 1.1 8.3 25.9 32.3 44.7 60.8 45.2 -27.4 21.4-61.8 31-96.4 27 28.8 18.5 63 29.2 99.8 29.2 120.8 0 189.1-102.1 185-193.6C399.9 193.1 410.9 181.7 419.6 168.6z"
-              ></path>
-            </svg>
-          </i>
-        </a>
-      </li>
     </ul>
     <full-page :options="options" id="fullpage" ref="fullpage">
       <div class="section">
@@ -98,7 +82,15 @@ export default {
           "page6",
           "page7",
         ],
-        sectionsColor: ["#E37B50", "#6D9DDF", "#E7D661", "#55BA63", "#A3A391"],
+        sectionsColor: [
+          "#F37344" /* page1 orange */,
+          "#5E9EE4" /* page2 blue */,
+          "#EBD547" /* page3 yellow */,
+          "#00BD58" /* page4 green */,
+          "#F37344",
+          "#EBD547",
+          "#5E9EE4",
+        ],
       },
     };
   },
@@ -129,8 +121,5 @@ ul {
 li {
   display: inline-block;
   margin: 0 10px;
-}
-a {
-  color: #42b983;
 }
 </style>
