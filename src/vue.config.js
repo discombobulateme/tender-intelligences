f12fae91904db4dbd0c12939bbdbc529c2af0a91
discombobulateme <paloma.oliveira@saucelabs.com>
2022-06-22 11:07:30 +0200

    Add build path

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line no-undef
 module.exports = defineConfig({
   chainWebpack: (config) => {
-    config.plugin('eslint').tap((args) => {
+    config.plugin("eslint").tap((args) => {
       args[0].fix = true;
       return args;
     });
   },
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/tender-intelligences/" : "/",
 });
