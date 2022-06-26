ac13372749789d606fbb2b637bc8b59402663d44
discombobulateme <paloma.oliveira@saucelabs.com>
2022-06-26 18:59:42 +0200

    Fix renatas email

--- src/views/TenderContact.vue
+++ src/views/TenderContact.vue
@@ -4,9 +4,7 @@
     <p>
       for questions or more information on the exhibition please reach out
       <a href="mailto:paloma@netzkunst.berlin">paloma oliveira</a> or
-      <a href="mailto:paloma@netzkunst.berlinrenata.gaui@gmail.com"
-        >renata gaui</a
-      >
+      <a href="mailto:renata.gaui@gmail.com">renata gaui</a>
     </p>
   </div>
 </template>
