f145ef5fc02eec1f8fbe9a642d44ce18aa59526d
discombobulateme <paloma.oliveira@saucelabs.com>
2022-06-30 17:40:15 +0200

    Add routes as outside const

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import TenderHome from "../views/TenderHome.vue";
+import ArtworkBbb from "../components/Artwork.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: TenderHome,
+  },
+  {
+    path: "/bbb-songs",
+    name: "bbb-songs",
+    component: ArtworkBbb,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/TenderAbout.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: TenderHome,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/TenderAbout.vue"),
-    },
-  ],
+  routes,
 });
 
 export default router;
