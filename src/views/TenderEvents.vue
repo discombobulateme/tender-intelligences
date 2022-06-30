0899c3f0070a94973efeaf52a4469c0ced7e4982
discombobulateme <paloma.oliveira@saucelabs.com>
2022-06-30 17:40:28 +0200

    Change events links, add map

--- src/views/TenderEvents.vue
+++ src/views/TenderEvents.vue
@@ -2,16 +2,24 @@
   <h3>events</h3>
   <div>
     <h4>Opening Reception 1</h4>
+    <p>Opening Reception I July 7th, 5pm PST (in person)</p>
     <p>
-      Opening Reception I July 7th, 5pm PST (in person)Mutable Studio - Los
-      Angeles
+      <a
+        href="https://goo.gl/maps/oqwARXBRgtCe2s5q7"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Mutable Studio @ 1206 Maple Ave, Suite 1032 Los Angeles</a
+      >
     </p>
-    <a href="mailto:">RSVP</a>
+    <p>No RSVP required</p>
   </div>
   <div>
     <h4>Dreaming Beyond AI Launch</h4>
     <p>Dreaming Beyond AI Launch July 7th, 5pm CEST (online)</p>
-    <a href="mailto:">RSVP</a>
+    <a
+      href="https://us06web.zoom.us/webinar/register/4516564939805/WN_QDUzK9bdT6OUy6DcFm2OVg"
+      >RSVP</a
+    >
   </div>
 </template>
 
