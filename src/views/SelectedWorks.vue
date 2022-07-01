7744569bc97bdccb87a5ca7a4e3d24749e97432a
discombobulateme <paloma.oliveira@saucelabs.com>
2022-07-01 13:47:18 +0200

    Add router links to selected works

--- src/views/SelectedWorks.vue
+++ src/views/SelectedWorks.vue
@@ -10,38 +10,67 @@
       by BBB_ (alla popp & Alex Sahm)
     </li>
     <li>
-      <h5>Queer Feminist AI</h5>
+      <h5>
+        <router-link :to="{ name: 'queer-feminist' }">
+          Queer Feminist AI
+        </router-link>
+      </h5>
       by alla popp
     </li>
     <li>
-      <h5>Art Search</h5>
+      <h5>
+        <router-link :to="{ name: 'art-search' }"> Art Search </router-link>
+      </h5>
       by Feminst.AI
     </li>
     <li>
-      <h5>Autonomous Bodies</h5>
+      <h5>
+        <router-link :to="{ name: 'autonomous-bodies' }">
+          Autonomous Bodies
+        </router-link>
+      </h5>
       by Feminst.AI
     </li>
     <li>
-      <h5>Contextual Normacy</h5>
+      <h5>
+        <router-link :to="{ name: 'contextual-normalcy' }"
+          >Contextual Normalcy</router-link
+        >
+      </h5>
       by poieto & Feminist.AI
     </li>
     <li>
-      <h5>Community of Intelligence</h5>
+      <h5>
+        <router-link :to="{ name: 'community-intelligence' }"
+          >Community of Intelligence</router-link
+        >
+      </h5>
       by Tabita Rezaire
     </li>
     <li>
-      <h5>Nichts als Solide (Nothing but Solid)</h5>
+      <h5>
+        <router-link :to="{ name: 'nichts-solide' }"
+          >Nichts als Solide (Nothing but Solid)</router-link
+        >
+      </h5>
       by Vanessa A. Opoku
     </li>
     <li>
       <h5>
-        Pensamento Computacional : Estratégia para a Educação Tecnológica
-        (Computational Thinking: Strategy for Technological Education)
+        <router-link :to="{ name: 'pensamento-computacional' }"
+          >Pensamento Computacional : Estratégia para a Educação Tecnológica
+          (Computational Thinking: Strategy for Technological
+          Education)</router-link
+        >
       </h5>
       by Nina da Hora
     </li>
     <li>
-      <h5>How machines discriminate</h5>
+      <h5>
+        <router-link :to="{ name: 'how-machines' }"
+          >How machines discriminate</router-link
+        >
+      </h5>
       by Nushin Yazdani and José Rojas
     </li>
   </ul>
