b2c86e91603cf071e5156b6489e2b26131771e73
discombobulateme <paloma.oliveira@saucelabs.com>
2022-07-02 00:04:04 +0200

    Add routes and default pages

---
+++ src/components/CommunityIntelligence.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <h3>BBB_ Songs of Cyborgeoisie</h3>
+  <p>
+    BBB_ Songs of Cyborgeoisie is a speculation about a queer evolutionary
+    scenario of technologies, people, flora and fauna on Earth. It narrates
+    about our world in the near future and enables a journey through feelings,
+    fears and hopes of the worlds beings.
+  </p>
+  <p>
+    Along with other inhabitants of the Robear Rehab - a rehabilitation center
+    in a fantasy Universe of Cyborgeoisie - the AI character shares trauma of
+    their feminine antagonist status quo in pop-cultural narratives on digital
+    technology and its prescribed role in technological futures.
+  </p>
+  <p>
+    While AGI is idolized and fetishised, injured or murdered, subordinated and
+    generally dramatised, varieties of artificial intelligence technologies
+    infiltrate all spheres of life, reinforce human bias, and thus foster
+    automated inequality in the present, making non-dystopian futures impossible
+    to imagine. What kind of narratives do we need to shift and alter
+    pop-cultural understanding of the artificial intelligences? How can such
+    stories be accessible to a wider audience, but still down to earth and
+    unsimplified?
+  </p>
+</template>
