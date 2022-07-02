6e31bd22a60fe41dcd28694ef6cb02893cc1468b
discombobulateme <paloma.oliveira@saucelabs.com>
2022-07-02 14:08:19 +0200

    Add background to components

--- src/components/CommunityIntelligence.vue
+++ src/components/CommunityIntelligence.vue
@@ -1,17 +1,20 @@
 <script setup></script>
 
 <template>
-  <h3>Community of Intelligence</h3>
-  <p class="fichatecnica">Sound installation, 2021</p>
-  <p>
-    A community of intelligence and the intelligence of community. A meditation
-    to become aware of all the communities that are sustained by the earth.
-    Living and non-living: Bacteria, fungus, virus, algae, plants, insects, sea
-    animals, land animals, birds, humans, machines, stones, rivers, mountains,
-    volcanoes, seas and oceans…maybe some extra-terrestrial life forms benefit
-    from the bounty of our planet … we are one of many communities doing their
-    best to survive, to thrive, seeking to find their place in the world.
-  </p>
-  <h4>about the artists</h4>
-  <p>Tabita Rezaire</p>
+  <div class="artworks">
+    <h3>Community of Intelligence</h3>
+    <p class="fichatecnica">Sound installation, 2021</p>
+    <p>
+      A community of intelligence and the intelligence of community. A
+      meditation to become aware of all the communities that are sustained by
+      the earth. Living and non-living: Bacteria, fungus, virus, algae, plants,
+      insects, sea animals, land animals, birds, humans, machines, stones,
+      rivers, mountains, volcanoes, seas and oceans…maybe some extra-terrestrial
+      life forms benefit from the bounty of our planet … we are one of many
+      communities doing their best to survive, to thrive, seeking to find their
+      place in the world.
+    </p>
+    <h4>about the artists</h4>
+    <p>Tabita Rezaire</p>
+  </div>
 </template>
