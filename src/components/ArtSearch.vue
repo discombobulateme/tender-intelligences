5360749d7ae22f2b50d1e2ff093de7a5f7f952a0
discombobulateme <paloma.oliveira@saucelabs.com>
2022-07-04 08:22:58 +0200

    Add info to work

--- src/components/ArtSearch.vue
+++ src/components/ArtSearch.vue
@@ -17,7 +17,32 @@
       linear narrative of progress, showing how each technological layer both
       illuminates and obscures our experience of information.
     </p>
-    <h4>about the artists</h4>
-    <p></p>
+    <br />
+    <h4>Art Search Community</h4>
+    <ul>
+      <li>Karina Lopez</li>
+      <li>Viv Pitter</li>
+      <li>Anna Cho-Son</li>
+      <li>Sai Tripathi</li>
+      <li>Laila Brown</li>
+      <li>Kryzia Monroy</li>
+    </ul>
+    <br />
+    <strong> Art Search Expanded Community</strong>
+    <p>
+      Our extended team includes folks new to the project or who have loosely
+      been working on the project over the past year.
+    </p>
+    <ul>
+      <li>Lily Cheng</li>
+      <li>Varshini Vijay</li>
+      <li>Anjni Shah</li>
+      <li>Elizabeth Cruz-Nakamura</li>
+      <li>Danielle Knutson</li>
+      <li>Sarah Cheung</li>
+      <li>Christine Meinders</li>
+      <li>Michelle Gong Matt</li>
+      <li>Asato-Adams</li>
+    </ul>
   </div>
 </template>
