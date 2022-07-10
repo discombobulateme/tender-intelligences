f4c448640c021e2d9bd856743629023191d3f316
Mateus Knelsen <sapientia_@hotmail.com>
2022-07-10 22:38:12 +0200

    Add Pinia store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, markRaw } from 'vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 
 import '@/styles/base.css'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+// Allow Pinia stores to access Vue Router instance:
+const pinia = createPinia()
+pinia.use(({ store }) => {
+  store.router = markRaw(router)
+})
+
+createApp(App).use(pinia).use(router).mount('#app')
---
+++ src/stores/appState.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+
+export const useAppStateStore = defineStore('appState', {
+  state: () => ({
+    transitioning: false,
+    pageHeight: 0,
+    menuOpen: false,
+  }),
+  getters: {},
+  actions: {
+    setTransitioning(transitioning) {
+      this.transitioning = transitioning
+    },
+    setPageHeight(height) {
+      this.pageHeight = height
+    },
+  },
+})
